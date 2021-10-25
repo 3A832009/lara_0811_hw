3a4cc68b9e130242554b500186e674cc9360f10f
Chen Pei An <3a832009@gm.student.ncut.edu.tw>
2021-10-25 22:52:39 +0800

    Route設置

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('about',[HomeController::class,'index'])->name('about.index');
+Route::get('news',[HomeController::class,'index'])->name('news.index');
